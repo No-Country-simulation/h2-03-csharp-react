d1dde2a456342923339d46f9fda8bbad5bd3c435
Rodri <rodrikohnen@gmail.com>
2024-11-01 18:02:07 -0300

    backend fix: add limit future bets

--- Backend/WakiBack.BLL/Services/Predictions/PredictionService.cs
+++ Backend/WakiBack.BLL/Services/Predictions/PredictionService.cs
@@ -37,8 +37,10 @@ namespace WakiBack.BLL
 
 
             var sumMatchPredictions = model.ListMatch.Count;
+            int sumMatchPredictionsFuture = 0;
             var todayDate = DateTime.Now;
             int limitBetsPerDay = 5;
+            int limitBetsPerDayFuture = 2;
 
             var customer = await _unitOfWork.Customers.GetByUserEmailAsync(email);
             if (customer == null) return GetBusinessResponse(HttpStatusCode.NotFound, "Customer not found");
@@ -78,8 +80,10 @@ namespace WakiBack.BLL
 
 
             var countPredictionsToday = predictionForDay!.CountBets;
-            var betsLeftForDay = limitBetsPerDay - countPredictionsToday;            
-            
+            var countPredictionsTodayForFuture = predictionForDay!.CountFutureBets;
+            var betsLeftForDay = limitBetsPerDay - countPredictionsToday;
+            var betsLeftForDayFuture = limitBetsPerDayFuture - countPredictionsTodayForFuture;
+
             if (countPredictionsToday >= limitBetsPerDay)
             {
                 return GetBusinessResponse(HttpStatusCode.BadRequest,
@@ -89,6 +93,40 @@ namespace WakiBack.BLL
             {
                 return GetBusinessResponse(HttpStatusCode.BadRequest,
                  $"The total of your daily bets, including your current bets, will exceed the limit of 5. You have {betsLeftForDay} bets left for today. If you wish to increase your limit, please consider upgrading your subscription.");
+            }
+
+            
+
+            foreach (var element in model.ListMatch!)
+            {
+                var match = await _unitOfWork.Matches.GetFirstOrDefaultMatchAsync(m => m.EntityPublicKey == element.MatchPublicKey);
+                if (match == null)
+                {
+                    return GetBusinessResponse(HttpStatusCode.BadRequest,
+                    $"Match with Public Key: {element.MatchPublicKey} not found");
+                }
+
+                if (DateTime.TryParse(match.Date, out DateTime matchDate))
+                {
+                    
+                    if (matchDate.Date != DateTime.Today)
+                    {
+                        sumMatchPredictionsFuture++;
+                    }
+                }
+                else
+                {
+                    return GetBusinessResponse(HttpStatusCode.BadRequest,
+                        $"Invalid date format for Match with Public Key: {element.MatchPublicKey}");
+                }
+
+            }
+       
+
+            if (countPredictionsTodayForFuture + sumMatchPredictionsFuture > limitBetsPerDayFuture)
+            {
+                return GetBusinessResponse(HttpStatusCode.BadRequest,
+                    $"You have reached the future bet limit. You can only place {betsLeftForDayFuture} more future bet(s) today. The rest of your bets must be on matches scheduled for today.");
             }
 
             var listMatchForPrediction = new List<MatchPrediction>();
@@ -131,6 +169,14 @@ namespace WakiBack.BLL
 
 
                 predictionForDay.CountBets ++;
+              
+               if (DateTime.TryParse(match.Date, out DateTime matchDate))
+               {                        
+                   if (predictionForDay.DateFirstBetOfDay.Date != matchDate.Date)
+                   {
+                       predictionForDay.CountFutureBets++;
+                   }
+               }
                 listMatchForPrediction.Add(matchForPrediction);
                 combinedRatio = combinedRatio == 0 ? matchForPrediction.RatioOfPrediction :
                 combinedRatio * matchForPrediction.RatioOfPrediction;                
