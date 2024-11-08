c5886acfc1a60553900c0c7a3e921c06253488a3
Wius <wius93@gmail.com>
2024-11-07 21:56:02 -0300

    fix(LandingPage): reubicar carpeta de imágenes y corregir importaciones en el slider

--- Frontend/src/components/layout/landpage/LpgTeamsSlider.tsx
+++ Frontend/src/components/layout/landpage/LpgTeamsSlider.tsx
@@ -1,158 +1,108 @@
 import "../../../styles/landpage.css";
+import arsenal from "../../../assets/teamscrest/arsenal.png";
+import astonvilla from "../../../assets/teamscrest/astonvilla.png";
+import atlmadrid from "../../../assets/teamscrest/atlmadrid.png";
+import barcelona from "../../../assets/teamscrest/barcelona.png";
+import boca from "../../../assets/teamscrest/boca.png";
+import chelsea from "../../../assets/teamscrest/chelsea.png";
+import independiente from "../../../assets/teamscrest/independiente.png";
+import inter from "../../../assets/teamscrest/inter.png";
+import juventus from "../../../assets/teamscrest/juventus.png";
+import liverpool from "../../../assets/teamscrest/liverpool.png";
+import manchesterCity from "../../../assets/teamscrest/manchestercity.png";
+import manchesterUnited from "../../../assets/teamscrest/manchesterunited.png";
+import milan from "../../../assets/teamscrest/milan.png";
+import napoli from "../../../assets/teamscrest/napoli.png";
+import newells from "../../../assets/teamscrest/newells.png";
+import racing from "../../../assets/teamscrest/racing.png";
+import realmadrid from "../../../assets/teamscrest/realmadrid.png";
+import river from "../../../assets/teamscrest/river.png";
+import roma from "../../../assets/teamscrest/roma.png";
+import rosariocentral from "../../../assets/teamscrest/rosariocentral.png";
+import sanLorenzo from "../../../assets/teamscrest/sanlorenzo.png";
+import sevilla from "../../../assets/teamscrest/sevilla.png";
+import tottenham from "../../../assets/teamscrest/tottenham.png";
+import valencia from "../../../assets/teamscrest/valencia.png";
+import velez from "../../../assets/teamscrest/velez.png";
 
 const LpgTeamsSlider = () => {
   return (
     <section className="block-slider-imgs">
       <div className="slider">
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/arsenal.png"
-            alt="escudo de Arsenal"
-          />
+          <img src={arsenal} alt="escudo de Arsenal" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/astonvilla.png"
-            alt="escudo de Aston Villa"
-          />
+          <img src={astonvilla} alt="escudo de Aston Villa" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/atlmadrid.png"
-            alt="escudo de Atletico Madrid"
-          />
+          <img src={atlmadrid} alt="escudo de Atletico Madrid" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/barcelona.png"
-            alt="escudo de Barcelona"
-          />
+          <img src={barcelona} alt="escudo de Barcelona" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/boca.png"
-            alt="escudo de Boca"
-          />
+          <img src={boca} alt="escudo de Boca" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/chelsea.png"
-            alt="escudo de Chelsea"
-          />
+          <img src={chelsea} alt="escudo de Chelsea" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/independiente.png"
-            alt="escudo de Independiente"
-          />
+          <img src={independiente} alt="escudo de Independiente" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/inter.png"
-            alt="escudo de Inter"
-          />
+          <img src={inter} alt="escudo de Inter" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/juventus.png"
-            alt="escudo de Juvestus"
-          />
+          <img src={juventus} alt="escudo de Juvestus" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/liverpool.png"
-            alt="escudo de Liverpool"
-          />
+          <img src={liverpool} alt="escudo de Liverpool" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/manchestercity.png"
-            alt="escudo de Manchester City"
-          />
+          <img src={manchesterCity} alt="escudo de Manchester City" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/manchesterunited.png"
-            alt="escudo de Manchester United"
-          />
+          <img src={manchesterUnited} alt="escudo de Manchester United" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/milan.png"
-            alt="escudo de Milan"
-          />
+          <img src={milan} alt="escudo de Milan" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/napoli.png"
-            alt="escudo de Napoli"
-          />
+          <img src={napoli} alt="escudo de Napoli" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/newells.png"
-            alt="escudo de Newells"
-          />
+          <img src={newells} alt="escudo de Newells" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/racing.png"
-            alt="escudo de Racing"
-          />
+          <img src={racing} alt="escudo de Racing" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/realmadrid.png"
-            alt="escudo de Real Madrid"
-          />
+          <img src={realmadrid} alt="escudo de Real Madrid" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/river.png"
-            alt="escudo de River"
-          />
+          <img src={river} alt="escudo de River" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/roma.png"
-            alt="escudo de Roma"
-          />
+          <img src={roma} alt="escudo de Roma" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/rosariocentral.png"
-            alt="escudo de Rosario Central"
-          />
+          <img src={rosariocentral} alt="escudo de Rosario Central" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/sanlorenzo.png"
-            alt="escudo de San Lorenzo"
-          />
+          <img src={sanLorenzo} alt="escudo de San Lorenzo" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/sevilla.png"
-            alt="escudo de Sevilla"
-          />
+          <img src={sevilla} alt="escudo de Sevilla" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/tottenham.png"
-            alt="escudo de Tottenham"
-          />
+          <img src={tottenham} alt="escudo de Tottenham" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/valencia.png"
-            alt="escudo de Valencia"
-          />
+          <img src={valencia} alt="escudo de Valencia" />
         </div>
         <div className="slide">
-          <img
-            src="../../../../public/teamscrest/velez.png"
-            alt="escudo de Velez"
-          />
+          <img src={velez} alt="escudo de Velez" />
         </div>
       </div>
     </section>
